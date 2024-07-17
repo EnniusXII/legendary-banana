cac7d5fdbd43da93b87129ec25d64a17baf980f2
EnniusXII <stefan.lahtinen@gmail.com>
2024-07-17 05:28:38 -0400

    add calculateBalance function

--- backend/models/Wallet.mjs
+++ backend/models/Wallet.mjs
@@ -18,4 +18,29 @@ export default class Wallet {
     
         return new Transaction({sender: this, recipient, amount});
     };
+
+    static calculateBalance({chain, address}) {
+        let total = 0;
+        let hasAddedTransaction = false;
+    
+        for (let i = chain.length - 1; i > 0; i--) {
+          const block = chain[i];
+    
+          for (let transaction of block.data) {
+            if (transaction.inputMap.address === address) {
+              hasAddedTransaction = true;
+            }
+    
+            const value = transaction.outputMap[address];
+    
+            if (value) {
+              total += value;
+            }
+          }
+    
+          if (hasAddedTransaction) break;
+        }
+    
+        return hasAddedTransaction ? total : INITIAL_BALANCE + total;
+      };
 }
